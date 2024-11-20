8f04116d2fbfcbff78a5523d86d7168dd94d10ed
bocugoz <kerem.bocugoz21@imperial.ac.uk>
2024-11-20 17:45:16 +0000

    comments for code

--- app.R
+++ app.R
@@ -1,35 +1,35 @@
+#loading in the libraries
 library(shiny)
 library(leaflet)
 library(glue)
-
 library(sp)
-
 library(sf)
 library(ggplot2)
-
 library(plotly)
+library(readr)
 
-# marci test
+#loading in the data
+my_sf <- read_sf("https://raw.githubusercontent.com/radoi90/housequest-data/refs/heads/master/london_boroughs.geojson") #london borough geolocational data
+borough_data <- read.csv("london-borough-profiles-2016 Data set.csv") #london borough data (population)
 
-my_sf <- read_sf("https://raw.githubusercontent.com/radoi90/housequest-data/refs/heads/master/london_boroughs.geojson")
-borough_data <- read.csv("london-borough-profiles-2016 Data set.csv")
+#merging the data
+colnames(borough_data)[colnames(borough_data) == "Area.name"] <- "name" #rename the columns to be the same 
+df <- merge(x = my_sf, y = borough_data, by = "name") #merge into one dataset
+df$GLA.Population.Estimate.2016 <- as.numeric(gsub(",", "", df$GLA.Population.Estimate.2016)) #get rid of commas in the data to convert to numeric data
+my_sf <- df #update my_sf
 
-colnames(borough_data)[colnames(borough_data) == "Area.name"] <- "name"
-df <- merge(x = my_sf, y = borough_data, by = "name")
-df$GLA.Population.Estimate.2016 <- as.numeric(gsub(",", "", df$GLA.Population.Estimate.2016))
-my_sf <- df
-
-ui <- fluidPage(
-    titlePanel("Urban Wellbeing Dashboard"),
+#building UI
+ui <- fluidPage( #create a responsive webpage
+    titlePanel("Urban Wellbeing Dashboard"), #page title
     sidebarLayout(
-        sidebarPanel(
+        sidebarPanel( #panel to add controls and information to the data product
             h2("Location Data"),
             p("Sensor at Westminster Bridge"),
             p("Latitude: 51.492248233302"),
             p("Longitude: -0.147114752900"),
             p(textOutput("pollution")),
             p(textOutput("time")),
-            radioButtons(
+            radioButtons( #to prioritize different factors we quantify in our data product
                 "test", "Factor Prioritisation:",
                 c(
                     "Population" = "pop",
@@ -37,7 +37,7 @@ ui <- fluidPage(
                 )
             ),
         ),
-        mainPanel(
+        mainPanel( #panel where the data visualisation happens
             h4("London"),
             plotlyOutput("plot2"),
             sliderInput(
@@ -45,35 +45,37 @@ ui <- fluidPage(
                 label = "Time at sensor:",
                 value = 0,
                 min = 0,
-                max = 22,
+                max = 24,
                 width = "100%"
             )
         )
     ),
 )
 
-library(readr)
-air_quality_data <- read_csv("open_aq_dataset.csv")
 
+air_quality_data <- read_csv("open_aq_dataset.csv") #load air quality data
+
+#server logic
 server <- function(input, output, session) {
     output$time <- renderText({
         time <- input$time_slider
-        glue("Time: {time}:00")
+        glue("Time: {time}:00") #whenever the slider is moved, it will display the new time
     })
     observeEvent(input$time_slider, {
         output$pollution <- renderText({
-            value <- air_quality_data$value[input$time_slider]
-            glue("No2 Reading: {value} µg/m³")
+            value <- air_quality_data$value[input$time_slider] #look up air quality value for that time
+            glue("No2 Reading: {value} µg/m³") #display desired air quality data 
         })
     })
 
+#creating the map
     output$plot2 <- renderPlotly({
         p <- ggplot() +
-            geom_sf(colour = "#241e55", data = my_sf, size = 1, linewidth = 0.15, aes(fill = GLA.Population.Estimate.2016)) +
+            geom_sf(colour = "#241e55", data = my_sf, size = 1, linewidth = 0.15, aes(fill = GLA.Population.Estimate.2016)) + 
             scale_fill_viridis_c(option = "cyan") +
-            coord_sf()
+            coord_sf() #plots the shape of boroughs and colours it based on population density
 
-        ggplotly(p)
+        ggplotly(p) #making the map interactive
     })
 }
 
