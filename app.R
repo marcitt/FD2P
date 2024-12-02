6d45c48e394c9d8dfda61ed818615e458db1f183
marcitt <marcitalbot23@gmail.com>
2024-12-02 14:24:06 +0000

    Plotting active sensors with latest sensor reading

--- app.R
+++ app.R
@@ -15,20 +15,66 @@ library(data.table)
 library(httr)
 library(jsonlite)
 
+library("stringr")
+
 api_key <- Sys.getenv("OPEN_AQ_API_KEY")
 
-res <- GET(
-    "https://api.openaq.org/v3/locations?coordinates=51.508045,-0.128217&radius=25000&limit=1000",
-    add_headers(`X-API-Key` = api_key)
-)
+# GET ALL SENSORS IN LONDON
+# res <- GET(
+#     "https://api.openaq.org/v3/locations?coordinates=51.508045,-0.128217&radius=25000&limit=1000",
+#     add_headers(`X-API-Key` = api_key)
+# )
 
-openaq_data <- fromJSON(rawToChar(res$content))
-results_data <- as.data.frame(openaq_data$results)
-output_dataframe <- as.data.frame(results_data)
+# FORMAT DATA
+# openaq_data <- fromJSON(rawToChar(res$content))
+# results_data <- as.data.frame(openaq_data$results)
+# output_dataframe <- as.data.frame(results_data)
 
-test <- output_dataframe$coordinates
-coordinates <- data.frame(output_dataframe$name, test$latitude, test$longitude)
-colnames(coordinates) <- c("name", "latitude", "longitude")
+# SAVE FULL DATAFRAME
+# write.csv(output_dataframe, "full_sensor_data.csv")
+
+# EXTRACT LONGITUDE, LATITUDE & RECORD OF LATEST READING
+# cords <- output_dataframe$coordinates
+# latest <- output_dataframe$datetimeLast
+# coordinates <- data.frame(output_dataframe$id, output_dataframe$name, cords$latitude, cords$longitude, latest$local)
+# colnames(coordinates) <- c("id", "name", "latitude", "longitude", "latest")
+
+# SAVE DATAFRAME
+# write.csv(coordinates, "sensor_data.csv")
+
+# GET CURRENT DATE
+# date <- Sys.Date()
+# str_date <- toString(date)
+
+# FILTER SENSORS RECENTLY TAKING RECORDINGS
+# sensor_data <- read.csv("sensor_data.csv")
+# current_sensor_data <- filter(sensor_data, str_sub(latest, 0, 10) == str_date)
+# write.csv(current_sensor_data, "current_sensor_data.csv")
+
+# # GET ALL SENSOR IDS
+# sensors <- read.csv("current_sensor_data.csv")
+# ids <- sensors$id
+
+# li <- list()
+
+# # ITERATE THROUGH IDS & GET FIRST SENSOR READING
+# for (element in ids) {
+#     res <- GET(
+#         glue("https://api.openaq.org/v3/locations/{element}/latest"),
+#         add_headers(`X-API-Key` = api_key)
+#     )
+#     openaq_data <- fromJSON(rawToChar(res$content))
+#     results_data <- as.data.frame(openaq_data$results)
+#     li <- append(li, results_data$value[1])
+# }
+
+# # ADD NEW COLUMN FOR SENSOR VALUES
+# sensors$value <- c(li)
+# sensors %>% filter(name == "Twickenham") #MANUALLY REMOVE OUTLIER
+
+# # https://stackoverflow.com/questions/24829027/unimplemented-type-list-when-trying-to-write-table
+# sensors <- apply(sensors, 2, as.character)
+# write.csv(sensors, "current_sensor_data_with_values.csv")
 
 
 my_sf <- read_sf("https://raw.githubusercontent.com/radoi90/housequest-data/refs/heads/master/london_boroughs.geojson")
@@ -72,6 +118,10 @@ ui <- fluidPage(
     ),
 )
 
+
+new <- read_csv("current_sensor_data_with_values.csv")
+new <- filter(new, name != "Twickenham")
+
 library(readr)
 air_quality_data <- read_csv("open_aq_dataset.csv")
 
@@ -92,7 +142,9 @@ server <- function(input, output, session) {
             geom_sf(colour = "#241e55", data = my_sf, size = 1, linewidth = 0.15, aes(color="red")) +
             # geom_sf(colour = "#241e55", data = my_sf, size = 1, linewidth = 0.15, aes(fill = GLA.Population.Estimate.2016)) +
             scale_fill_viridis_c(option = "cyan") +
-            geom_point(data = coordinates, mapping=aes(x=longitude, y=latitude)) +
+            # geom_point(data = coordinates, mapping=aes(x=longitude, y=latitude, color="all sensors")) +
+            geom_point(data = new, mapping=aes(x=longitude,y=latitude, color=value)) +
+            # geom_point(data = current_sensor_data, mapping=aes(x=longitude,y=latitude, color="active sensors")) +
             coord_sf()
 
         ggplotly(p)
