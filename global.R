f7461258feb60e860c37e51da95414cc9991de6d
marcitt <marcitalbot23@gmail.com>
2024-12-14 22:38:31 +0000

    New data prep method test

--- global.R
+++ global.R
@@ -18,70 +18,72 @@ library("stringr")
 
 api_key <- Sys.getenv("OPEN_AQ_API_KEY")
 
-# LIVE AIR QUALITY
-get_london_air_quality <- function() {
-    res <- GET(
-        # get all sensors within a radius of 25000
-        "https://api.openaq.org/v3/locations?coordinates=51.508045,-0.128217&radius=25000&limit=1000",
-        add_headers(`X-API-Key` = api_key)
-    )
+# # LIVE AIR QUALITY
+# get_london_air_quality <- function() {
+#     res <- GET(
+#         # get all sensors within a radius of 25000
+#         "https://api.openaq.org/v3/locations?coordinates=51.508045,-0.128217&radius=25000&limit=1000",
+#         add_headers(`X-API-Key` = api_key)
+#     )
 
-    openaq_data <- fromJSON(rawToChar(res$content))
-    results <- as.data.frame(openaq_data$results)
+#     openaq_data <- fromJSON(rawToChar(res$content))
+#     results <- as.data.frame(openaq_data$results)
 
-    cords <- results$coordinates
-    latest <- results$datetimeLast
-    df <- data.frame(results$id, results$name, cords$latitude, cords$longitude, latest$local)
-    colnames(df) <- c("id", "name", "latitude", "longitude", "latest")
+#     cords <- results$coordinates
+#     latest <- results$datetimeLast
+#     df <- data.frame(results$id, results$name, cords$latitude, cords$longitude, latest$local)
+#     colnames(df) <- c("id", "name", "latitude", "longitude", "latest")
 
-    write.csv(df, "data/air_quality/london_sensor_data_full.csv")
-}
+#     write.csv(df, "data/air_quality/london_sensor_data_full.csv")
+# }
 
-filter_latest_air_quality <- function(file_path, str_date) {
-    sensor_data <- read.csv(file_path)
-    current_sensor_data <- filter(sensor_data, str_sub(latest, 0, 10) == str_date)
-    write.csv(current_sensor_data, glue("data/air_quality/london_sensor_data_{str_date}.csv"))
-}
+# filter_latest_air_quality <- function(file_path, str_date) {
+#     sensor_data <- read.csv(file_path)
+#     current_sensor_data <- filter(sensor_data, str_sub(latest, 0, 10) == str_date)
+#     write.csv(current_sensor_data, glue("data/air_quality/london_sensor_data_{str_date}.csv"))
+# }
 
-get_sensor_values <- function(file_path, str_date) {
-    sensors <- read.csv(file_path)
-    ids <- sensors$id
+# get_sensor_values <- function(file_path, str_date) {
+#     sensors <- read.csv(file_path)
+#     ids <- sensors$id
 
-    li <- list()
+#     li <- list()
 
-    # iterate through ids and get first sensor reading
-    for (element in ids) {
-        res <- GET(
-            glue("https://api.openaq.org/v3/locations/{element}/latest"),
-            add_headers(`X-API-Key` = api_key)
-        )
-        openaq_data <- fromJSON(rawToChar(res$content))
-        results_data <- as.data.frame(openaq_data$results)
-        li <- append(li, results_data$value[1]) # first sensor reading in list
-    }
+#     # iterate through ids and get first sensor reading
+#     for (element in ids) {
+#         res <- GET(
+#             glue("https://api.openaq.org/v3/locations/{element}/latest"),
+#             add_headers(`X-API-Key` = api_key)
+#         )
+#         openaq_data <- fromJSON(rawToChar(res$content))
+#         results_data <- as.data.frame(openaq_data$results)
+#         li <- append(li, results_data$value[1]) # first sensor reading in list
+#     }
 
-    # add new column for sensor values
-    sensors$value <- c(li)
+#     # add new column for sensor values
+#     sensors$value <- c(li)
 
-    # https://stackoverflow.com/questions/24829027/unimplemented-type-list-when-trying-to-write-table
-    sensors <- apply(sensors, 2, as.character)
-    write.csv(sensors, glue("data/air_quality/london_sensor_data_values_{str_date}.csv"))
-}
+#     # https://stackoverflow.com/questions/24829027/unimplemented-type-list-when-trying-to-write-table
+#     sensors <- apply(sensors, 2, as.character)
+#     write.csv(sensors, glue("data/air_quality/london_sensor_data_values_{str_date}.csv"))
+# }
 
-# get current date
-date <- Sys.Date()
-str_date <- toString(date)
+# # get current date
+# date <- Sys.Date()
+# str_date <- toString(date)
 
-# check if sensor values have been computed for this day: 
-file_path <- glue("data/air_quality/london_sensor_data_values_{str_date}.csv")
-if (file.exists(file_path)) {
-   air_quality <- read_csv(glue("data/air_quality/london_sensor_data_values_{str_date}.csv"))
-} else {
-    get_london_air_quality()
-    filter_latest_air_quality("data/air_quality/london_sensor_data_full.csv", str_date)
-    get_sensor_values(glue("data/air_quality/london_sensor_data_{str_date}.csv"), str_date)
-    air_quality <- read_csv(glue("data/air_quality/london_sensor_data_values_{str_date}.csv"))
-}
+# # check if sensor values have been computed for this day: 
+# file_path <- glue("data/air_quality/london_sensor_data_values_{str_date}.csv")
+# if (file.exists(file_path)) {
+#    air_quality <- read_csv(glue("data/air_quality/london_sensor_data_values_{str_date}.csv"))
+# } else {
+#     get_london_air_quality()
+#     filter_latest_air_quality("data/air_quality/london_sensor_data_full.csv", str_date)
+#     get_sensor_values(glue("data/air_quality/london_sensor_data_{str_date}.csv"), str_date)
+#     air_quality <- read_csv(glue("data/air_quality/london_sensor_data_values_{str_date}.csv"))
+# }
+
+air_quality <- read.csv("data/air_quality/london-filtered-sensors-2024-12-14.csv")
 
 # Load greenspace shapefile
 greenspace <- st_read("data/GiGL_SpacesToVisit_Open_Shp/GiGL_SpacesToVisit_region.shp") # Load greenspace shapefile
