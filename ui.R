9e7bb2e327cc9d238b2e4dd721d705423bb3c626
bocugoz <kerem.bocugoz21@imperial.ac.uk>
2024-12-17 06:15:52 +0300

    added ux text

--- ui.R
+++ ui.R
@@ -19,18 +19,26 @@ ui <- fluidPage(
             h2("Personalised Wellbeing Metric"),
             h3("Your personalised wellbeing metric includes a prioritization slider to help you choose how much the metric is affected by Greenspace proximity and PM2.5 density. The treshold slider helps you navigate the best places for your ideal Wellbeing weighting!"),
             
-            # Prioritisation Slider with Icons
+            # Prioritisation Slider with Icons and Text Underneath
             tags$div(class = "slider-wrapper",
                 tags$img(src = "tree_icon.svg", class = "icon-left"),  # Left icon
                 sliderInput("wellbeing_slider", "Wellbeing Metric Prioritisation" , min = 0, max = 100, value = 50, post = "% Air Quality"),  # Slider
-                tags$img(src = "wind_icon.svg", class = "icon-right")  # Right icon
+                tags$img(src = "wind_icon.svg", class = "icon-right"),  # Right icon
+                tags$div(class = "slider-text",  # New text row
+                    tags$span("Proximity to Greenspace", class = "left-text"),
+                    tags$span("Absence of PM2.5", class = "right-text")
+                )
             ),
-            
-            # Threshold Slider with Icons
+
+            # Threshold Slider with Icons and Text Underneath
             tags$div(class = "slider-wrapper",
                 tags$img(src = "brain_fog_icon.svg", class = "icon-left"),  # Left icon
-                sliderInput("threshold_slider", "Wellbeing Metric Treshold" , min = 0, max = 10, value = 0, step = 0.1),  # Slider
-                tags$img(src = "wellbeing_icon.svg", class = "icon-right")  # Right icon
+                sliderInput("threshold_slider", "Wellbeing Metric Threshold" , min = 0, max = 10, value = 0, step = 0.1),  # Slider
+                tags$img(src = "wellbeing_icon.svg", class = "icon-right"),  # Right icon
+                tags$div(class = "slider-text",  # New text row
+                    tags$span("Anywhere in London", class = "left-text"),
+                    tags$span("Best places for outdoor wellbeing", class = "right-text")
+                )
             )
         ),
         
